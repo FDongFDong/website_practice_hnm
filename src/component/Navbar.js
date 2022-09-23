523e4c1b49755aaedbaa484e33fd44b946c7f481
FdongFdong <abnormal5626@naver.com>
2022-09-23 18:10:41 +0900

    [FEAT] navigation bar - 검색창 추가

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
+  const menuList = [
+    '여성',
+    'Divided',
+    '남성',
+    '신생아/유아',
+    '아동',
+    'H&M Home',
+    'Sale',
+    '지속가능성',
+  ];
   return (
     <div>
       <div>
@@ -11,8 +22,27 @@ const Navbar = () => {
           <div>로그인</div>
         </div>
       </div>
-      <div></div>
-      <div></div>
+      <div className="nav-section">
+        <img
+          width={100}
+          src="https://logos-world.net/wp-content/uploads/2020/04/HM-Logo-700x394.png"
+        />
+      </div>
+      <div className="menu-area">
+        <ul className="menuList">
+          {menuList.map((menu) => {
+            return <li>{menu}</li>;
+          })}
+        </ul>
+        <div className="search-area">
+          <div className="font-area">
+            <FontAwesomeIcon icon={faSearch} />
+          </div>
+          <div>
+            <input type="text" placeholder="제품검색" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
