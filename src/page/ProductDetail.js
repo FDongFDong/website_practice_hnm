2f86439ca074e9d7b92d288d6984fd7063949129
FdongFdong <abnormal5626@naver.com>
2022-09-24 18:20:23 +0900

    [FEAT] Add 상품 디테일 페이지 기능 추가 각 상품에 대한 이름, 사이즈 등 확인가능 하도록 추가

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,18 +1,50 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Container, Dropdown, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 const ProductDetail = () => {
   let { id } = useParams();
+  const [product, setProduct] = useState(null);
   const getProductDetail = async () => {
-    console.log('?');
     let url = `http://localhost:3004/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
-    console.log('asdasd', data);
+    setProduct(data);
   };
   useEffect(() => {
     getProductDetail();
   }, []);
-  return <div></div>;
+
+  const choiceDropdown = (event) => {
+    console.log('choiceDropdown =>', event.target);
+  };
+  return (
+    <Container>
+      <Row>
+        <Col className="product-img">
+          <img src={product?.img}></img>
+        </Col>
+        <Col>
+          <div>{product?.title}</div>
+          <div>₩{product?.price}</div>
+          <div>{product?.choice === true ? 'Conscious choice' : ''}</div>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              사이즈 선택
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu onClick={(event) => choiceDropdown(event)}>
+              {product?.size.map((size) => {
+                return <Dropdown.Item href="#/action-1">{size}</Dropdown.Item>;
+              })}
+            </Dropdown.Menu>
+          </Dropdown>
+          <div>
+            <Button variant="dark">추가</Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default ProductDetail;
